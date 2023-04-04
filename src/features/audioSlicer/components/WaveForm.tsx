ffe552ee41ca5737b0152146f1a94e372a7e92e9
Jeffrey King <jking@hey.com>
2023-04-04 20:44:28 +0800

    add ability to add slices and play those slices

--- src/features/audioSlicer/components/WaveForm.tsx
+++ src/features/audioSlicer/components/WaveForm.tsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 // @ts-ignore - need WaveSurfer type to satsify types here
 import { WaveSurfer as WaveSurferType } from "wavesurfer.js";
-import { PlayCircle, PauseCircle } from "react-feather";
+import { PlayCircle, PauseCircle, Plus, StopCircle } from "react-feather";
 type Props = {
   audioUrl: string | null;
 };
@@ -9,18 +9,46 @@ type Props = {
 // With only one region allowed, setting a REGION_ID is fine.
 const REGION_ID = "SINGLE_REGION_ID";
 
-const defaultRegion = {
-  id: REGION_ID,
-  start: 0,
-  end: 3,
-  loop: false,
+type AudioSlice = {
+  id: string;
+  start: number;
+  end: number;
+  loop: Boolean;
+  color: string;
 };
+
+function random_rgba() {
+  var o = Math.round,
+    r = Math.random,
+    s = 255;
+  return (
+    "rgba(" +
+    o(r() * s) +
+    "," +
+    o(r() * s) +
+    "," +
+    o(r() * s) +
+    "," +
+    "0.2" +
+    ")"
+  );
+}
+
+var color = random_rgba();
 
 const WaveForm = ({ audioUrl }: Props) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const [isPlaying, toggleIsPlaying] = useState(false);
+  const [isPlayingFullTrack, toggleIsPlayingFullTrack] = useState(false);
+  const [currentlyPlayingRegion, setCurrentlyPlayingRegion] = useState<
+    string | null
+  >(null);
+
+  const isPlaying = isPlayingFullTrack || Boolean(currentlyPlayingRegion);
+
   const [waveSurferObject, setWaveSurferObject] =
     useState<WaveSurferType>(null);
+
+  const [audioSlices, setAudioSlices] = useState<AudioSlice[]>([]);
 
   const create = async () => {
     const WaveSurfer = (await import("wavesurfer.js")).default;
@@ -44,13 +72,14 @@ const WaveForm = ({ audioUrl }: Props) => {
 
     waveSurfer.load(audioUrl);
     waveSurfer.on("ready", () => {
-      const audioEnd = waveSurfer.getDuration();
       waveSurfer.clearRegions();
-      waveSurfer.addRegion({ ...defaultRegion, end: audioEnd });
+      setAudioSlices([]);
+      waveSurfer.enableDragSelection();
     });
     waveSurfer.on("pause", () => {
-      toggleIsPlaying(false);
+      setCurrentlyPlayingRegion(null);
     });
+
     setWaveSurferObject(waveSurfer);
 
     return () => {
@@ -58,29 +87,35 @@ const WaveForm = ({ audioUrl }: Props) => {
     };
   };
 
-  const playRegion = () => {
-    const { start, end } = waveSurferObject.regions.list[REGION_ID];
-    const currentTime = waveSurferObject.getCurrentTime();
-
-    // custom logic in case user pauses in middle of play. We don't want to play from the beginning of the region again, which is what region.list[REGION_ID].play() defaults to
-    if (currentTime < end && currentTime > start) {
-      waveSurferObject.play(currentTime, end);
-    } else {
-      waveSurferObject.regions.list[REGION_ID].play();
-    }
-  };
-
-  const pauseRegion = () => {
-    waveSurferObject.pause();
-  };
-
-  const playPause = () => {
+  // Don't use playPause here because playing the slices also triggers playing. The play/pause can get out of sync
+  const playPauseFullTrack = () => {
     if (isPlaying) {
-      pauseRegion();
+      waveSurferObject.pause();
     } else {
-      playRegion();
+      waveSurferObject.play();
     }
-    toggleIsPlaying(!isPlaying);
+
+    // Reference !isPlaying here since we want to pause and play even if regions are playing. isPlaying looks at both if played from a region or from the start.
+    toggleIsPlayingFullTrack(!isPlaying);
+  };
+
+  const addNewSlice = (id: string, color: string) => {
+    const newRegion = { id, start: 0, end: 3, loop: false, color };
+    waveSurferObject.addRegion(newRegion);
+    setAudioSlices([...audioSlices, newRegion]);
+  };
+
+  const playStopRegion = (id: string) => {
+    if (currentlyPlayingRegion === id) {
+      setCurrentlyPlayingRegion(null);
+      waveSurferObject.stop();
+      waveSurferObject.setCurrentTime(waveSurferObject.regions.list[id].start);
+    } else {
+      waveSurferObject.stop();
+      console.log(" in else");
+      setCurrentlyPlayingRegion(id);
+      waveSurferObject.regions.list[id].play();
+    }
   };
 
   useEffect(() => {
@@ -90,21 +125,55 @@ const WaveForm = ({ audioUrl }: Props) => {
   return (
     <>
       {audioUrl && (
-        <div className="flex">
-          <button
-            className="flex-none"
-            onClick={() => {
-              return playPause();
-            }}
-            type="button"
-          >
-            {isPlaying ? <PauseCircle size={48} /> : <PlayCircle size={48} />}
-          </button>
-          <div className="flex-initial w-full">
-            <div className="w-full" ref={containerRef} />
+        <>
+          <div className="flex">
+            <button
+              className="flex-none"
+              onClick={() => {
+                return playPauseFullTrack();
+              }}
+              type="button"
+            >
+              {isPlaying ? <PauseCircle size={48} /> : <PlayCircle size={48} />}
+            </button>
+            <div className="flex-initial w-full">
+              <div className="w-full" ref={containerRef} />
+            </div>
           </div>
-        </div>
+          <div className="m-4">
+            <button
+              className="flex items-center bg-lime-200 hover:bg-lime-300 text-black font-bold py-2 px-4 rounded-full"
+              onClick={() => {
+                addNewSlice(crypto.randomUUID(), random_rgba());
+              }}
+            >
+              <Plus size={24} />
+              <span>Add New Slice</span>
+            </button>
+          </div>
+        </>
       )}
+      <div className="ml-24">
+        {audioSlices.map((slice, index) => {
+          return (
+            <div key={slice.id} className="flex items-center">
+              <button
+                key={slice.id}
+                onClick={() => {
+                  playStopRegion(slice.id);
+                }}
+              >
+                {slice.id === currentlyPlayingRegion ? (
+                  <StopCircle fill={slice.color} size={48} />
+                ) : (
+                  <PlayCircle fill={slice.color} size={48} />
+                )}
+              </button>
+              <span className="ml-4">Slice {index + 1}</span>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
