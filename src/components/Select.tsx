5caa0f61264f26377b6528af25cb12ac96afbd5a
Jeffrey King <jking@hey.com>
2023-04-08 10:16:01 +0800

    add demo

---
+++ src/components/Select.tsx
@@ -0,0 +1,48 @@
+type Option = {
+  value: string;
+  text: string;
+};
+
+type Props = {
+  defaultText: string;
+  options: Option[];
+  handleSelect: (event: string) => void;
+  selectedValue: string;
+};
+
+const Select = ({
+  defaultText,
+  options,
+  handleSelect,
+  selectedValue,
+}: Props) => {
+  return (
+    <>
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        {defaultText}
+      </label>
+      <select
+        id="countries"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(event) => {
+          console.log({ event: event.target.value });
+          handleSelect(event.target.value);
+        }}
+      >
+        {options.map((option) => {
+          return (
+            <option
+              key={option.value}
+              value={option.value}
+              selected={option.value === selectedValue}
+            >
+              {option.text}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
+};
+
+export default Select;
---
+++ src/pages/demo/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import WaveForm from "src/features/audioSlicer/components/WaveForm";
+import Select from "src/components/Select";
+
+const options = [
+  {
+    text: "Taiwanese Lesson 1",
+    value: "taigi_lesson1.m4a",
+  },
+  {
+    text: "Taiwanese Lesson 3",
+    value: "taigi_lesson3.m4a",
+  },
+];
+
+const DemoContainer = () => {
+  const [selectedAudio, setSelectedAudio] = useState(options[0].value);
+
+  const handleSelect = (value: string) => {
+    setSelectedAudio(value);
+  };
+
+  return (
+    <div className="w-8/12 m-auto mt-8 p-8">
+      <div className="w-1/4 m-auto">
+        <Select
+          options={options}
+          defaultText="Select Demo Audio"
+          handleSelect={handleSelect}
+          selectedValue={selectedAudio}
+        />
+      </div>
+      <div className="p-8">
+        <WaveForm audioUrl={selectedAudio} />
+      </div>
+    </div>
+  );
+};
+
+export default DemoContainer;
