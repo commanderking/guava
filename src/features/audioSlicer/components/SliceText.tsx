5271ddf9a2e8f95c0c56a2610a47b5a6925b01f9
Jeffrey King <jking@hey.com>
2023-04-11 21:29:30 +0800

    add ability to show and edit text

---
+++ src/features/audioSlicer/components/SliceText.tsx
@@ -0,0 +1,49 @@
+import { CheckCircle, Edit } from "react-feather";
+import { useState } from "react";
+
+type Props = {
+  text: string;
+  isEditing: boolean;
+  handleEdit: () => void;
+  handleSave: (text: string) => void;
+};
+
+const SliceText = ({ text, isEditing, handleEdit, handleSave }: Props) => {
+  const [value, setValue] = useState(text);
+
+  return (
+    <>
+      {isEditing ? (
+        <input
+          type="text"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleSave(value);
+            }
+          }}
+        ></input>
+      ) : (
+        <span className="ml-4">{text}</span>
+      )}
+      <button
+        onClick={() => {
+          if (isEditing) {
+            handleSave(value);
+          } else {
+            handleEdit();
+          }
+        }}
+        className="ml-4"
+      >
+        {isEditing ? <CheckCircle size={24} /> : <Edit size={24} />}
+      </button>
+    </>
+  );
+};
+
+export default SliceText;
