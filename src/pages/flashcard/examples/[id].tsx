5b2efb318b250d60526fd6999cb987d457bb721d
Jeffrey King <jking@hey.com>
2023-10-05 20:06:14 +0800

    create table for the list of items

--- src/pages/flashcard/examples/[id].tsx
+++ src/pages/flashcard/examples/[id].tsx
@@ -2,7 +2,7 @@ import { getAllFlashcardIds, getFlashcardCsv } from "@/lib/flashcard";
 import FlashcardContainer from "src/features/flashcard/Container";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { Card } from "src/features/flashcard/types";
-
+import VocabTableContainer from "src/features/vocabTable/Container";
 type Props = {
   cards: Card[];
 };
@@ -10,9 +10,11 @@ type Props = {
 export default function Post({
   cards,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log({ cards });
   return (
     <div>
       <FlashcardContainer initialFlashcards={cards} />
+      <VocabTableContainer rowData={cards} />
     </div>
   );
 }
